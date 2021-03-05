63fb5288e9cb630ea8a7563b30f013e0e311941b
amercehic <cehic.am@gmail.com>
2021-03-05 12:57:25 +0100

    added auth to post patient

--- src/routes/v1/patient.route.js
+++ src/routes/v1/patient.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const validate = require('../../middlewares/validate');
+const auth = require('../../middlewares/auth');
 const patientController = require('../../controllers/patient.controller');
 const patientValidation = require('../../validations/patient.validation');
 
 const router = express.Router();
 
-router.post('/create', validate(patientValidation.create), patientController.createPatient);
+router.route('/').post(auth('/'), validate(patientValidation.create), patientController.createPatient);
 
 module.exports = router;
