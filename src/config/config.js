587647b347f9fdf7df3d7a1e9d60777d92a78365
amercehic <cehic.am@gmail.com>
2021-03-08 21:12:48 +0100

    mongo changeS

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
   mongoose: {
-    url: process.env.MONGODB_URL + (process.env.NODE_ENV === 'test' ? '-test' : ''),
+    url: process.env.MONGODB_URL,
     options: {
       useCreateIndex: true,
       useNewUrlParser: true,
